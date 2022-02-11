8c833b3768447b00203ba4020cdc5b5defbd64f2
DEV-COM <kwp@tlen.pl>
2022-02-11 08:23:40 +0100

    piątek, g.9

--- start.py
+++ start.py
@@ -1,8 +1,8 @@
-
-from mojpakiet import *
-
-
-lista_programow_do_instalacji = ['requests', 'httpx==0.18.2']
-
-install_packages(lista_programow_do_instalacji)
+#
+# from mojpakiet import *
+#
+#
+# lista_programow_do_instalacji = ['requests', 'httpx==0.18.2']
+#
+# install_packages(lista_programow_do_instalacji)
 
